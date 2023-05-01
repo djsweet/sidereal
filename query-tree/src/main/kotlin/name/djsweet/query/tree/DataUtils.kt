bdb2cbe09ccdfb45980e7a338c3be8377b7adec9
Dani Sweet <danieljsweet@icloud.com>
2023-05-01 19:22:20 -0400

    Use specialized MapSequenceIterator for mapSequence

--- query-tree/src/main/kotlin/name/djsweet/query/tree/DataUtils.kt
+++ query-tree/src/main/kotlin/name/djsweet/query/tree/DataUtils.kt
@@ -15,12 +15,25 @@ fun <T>workingDataForAvailableKeys(fullData: QPTrie<ByteArray>, keyDispatch: QPT
     return result
 }
 
+internal class MapSequenceIterator<U, V>(
+    private val basis: Iterator<U>,
+    private val functor: (item: U) -> V
+): Iterator<V> {
+    override fun hasNext(): Boolean {
+        return this.basis.hasNext()
+    }
+
+    override fun next(): V {
+        return this.functor(this.basis.next())
+    }
+}
+
 internal fun <U, V> mapSequence(iterable: Iterable<U>, functor: (item: U) -> V): Iterator<V> {
-    return iterable.asSequence().map(functor).iterator()
+    return MapSequenceIterator(iterable.iterator(), functor)
 }
 
 internal fun <U, V> mapSequence(iterator: Iterator<U>, functor: (item: U) -> V): Iterator<V> {
-    return iterator.asSequence().map(functor).iterator()
+    return MapSequenceIterator(iterator, functor)
 }
 
 internal fun compareBytesUnsigned(left: Byte, right: Byte): Int {
