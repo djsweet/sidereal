1c46bc659f15a55694976f85afce7a6f38ee47db
Dani Sweet <danieljsweet@icloud.com>
2023-11-16 13:25:27 -0500

    More complete tests ensuring key/value pair size limits prevent StackOverflowError

--- thorium/src/test/kotlin/name/djsweet/thorium/KeyValueSizeLimitsTest.kt
+++ thorium/src/test/kotlin/name/djsweet/thorium/KeyValueSizeLimitsTest.kt
@@ -4,7 +4,8 @@ import io.vertx.core.Vertx
 import io.vertx.kotlin.coroutines.awaitResult
 import kotlinx.coroutines.runBlocking
 import name.djsweet.query.tree.QPTrie
-import org.junit.jupiter.api.Assertions.assertEquals
+import name.djsweet.query.tree.QPTrieKeyValue
+import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
 
 // If either of these fail,
@@ -24,6 +25,19 @@ import org.junit.jupiter.api.Test
 // cause us to go over the budget.
 
 class KeyValueSizeLimitsTest {
+    companion object {
+        private fun <T> validateIterator(
+            iterator: Iterator<QPTrieKeyValue<T>>,
+            visitor: (handleEntry: (QPTrieKeyValue<T>) -> Unit) -> Unit
+        ) {
+            visitor {
+                assertTrue(iterator.hasNext())
+                assertEquals(iterator.next(), it)
+            }
+            assertFalse(iterator.hasNext())
+        }
+    }
+
     private fun keyValueSizeLimitImpl(sizeLimit: Int) {
         var trie = QPTrie<Int>()
         for (size in 1..sizeLimit) {
@@ -38,6 +52,67 @@ class KeyValueSizeLimitsTest {
             //println("ok for size=$size budget=$sizeLimit heap=${Runtime.getRuntime().maxMemory()}")
         }
         assertEquals((sizeLimit * 2).toLong(), trie.size)
+
+        // The visitation methods should similarly not trigger a StackOverflowError when run with the size limit.
+        // The functionality below is already semantically tested under the query.tree package, but we're running it
+        // again here for the side effect of stack consumption.
+        val minKvp = trie.minKeyValueUnsafeSharedKey()
+        assertNotNull(minKvp)
+        assertEquals(sizeLimit * 2 + 1, minKvp!!.value)
+        assertEquals(sizeLimit - 1, minKvp.key.indexOfFirst { it != 0.toByte() })
+
+        val indexMidpoint = sizeLimit / 2
+        var kvpAtIndexMidpoint: QPTrieKeyValue<Int>? = null
+        var kvpAtLastIndex: QPTrieKeyValue<Int>? = null
+
+        // This is a special case because of how we're complecting the midpoint and last index saves,
+        // so it can't use validateIterator directly.
+        val fullIteratorUnspecified = trie.iteratorUnsafeSharedKey()
+        trie.visitUnsafeSharedKey {
+            assertTrue(fullIteratorUnspecified.hasNext())
+            assertEquals(fullIteratorUnspecified.next(), it)
+
+            // This is a bit of an overload: we want to perform inequality-based iteration in later tests,
+            // so we pick the value at the middle of the input set, and the value at the end of the input set, to use as
+            // the basis for these tests.
+            if (it.value == indexMidpoint) {
+                kvpAtIndexMidpoint = it
+            }
+            kvpAtLastIndex = it
+        }
+        assertFalse(fullIteratorUnspecified.hasNext())
+
+        validateIterator(trie.iteratorAscendingUnsafeSharedKey()) {
+            trie.visitAscendingUnsafeSharedKey(it)
+        }
+
+        validateIterator(trie.iteratorDescendingUnsafeSharedKey()) {
+            trie.visitDescendingUnsafeSharedKey(it)
+        }
+
+        assertNotNull(kvpAtIndexMidpoint)
+        assertNotNull(kvpAtLastIndex)
+
+        validateIterator(trie.iteratorLessThanOrEqualUnsafeSharedKey(kvpAtIndexMidpoint!!.key)) {
+            trie.visitLessThanOrEqualUnsafeSharedKey(kvpAtIndexMidpoint!!.key, it)
+        }
+
+        validateIterator(trie.iteratorLessThanOrEqualUnsafeSharedKey(kvpAtLastIndex!!.key)) {
+            trie.visitLessThanOrEqualUnsafeSharedKey(kvpAtLastIndex!!.key, it)
+        }
+
+        validateIterator(trie.iteratorStartsWithUnsafeSharedKey(kvpAtIndexMidpoint!!.key)) {
+            trie.visitStartsWithUnsafeSharedKey(kvpAtIndexMidpoint!!.key, it)
+        }
+
+        validateIterator(trie.iteratorGreaterThanOrEqualUnsafeSharedKey(kvpAtIndexMidpoint!!.key)) {
+            trie.visitGreaterThanOrEqualUnsafeSharedKey(kvpAtIndexMidpoint!!.key, it)
+        }
+
+        val emptyByteArray = byteArrayOf()
+        validateIterator(trie.iteratorGreaterThanOrEqualUnsafeSharedKey(emptyByteArray)) {
+            trie.visitGreaterThanOrEqualUnsafeSharedKey(emptyByteArray, it)
+        }
     }
 
     @Test
