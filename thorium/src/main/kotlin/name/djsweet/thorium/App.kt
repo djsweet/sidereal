9711ceffc719f871802db32445b371989649a0f2
Dani Sweet <danieljsweet@icloud.com>
2023-11-21 10:28:48 -0500

    Extract common environment variable prefix

--- thorium/src/main/kotlin/name/djsweet/thorium/App.kt
+++ thorium/src/main/kotlin/name/djsweet/thorium/App.kt
@@ -21,6 +21,7 @@ import kotlin.system.exitProcess
 
 // FIXME: Set this elsewhere
 private const val versionString = "0.8.0"
+private const val envVarPrefix = "THORIUM_"
 
 internal class KvpByteBudgetCommand: CliktCommand(
     help="Determines the maximum key/value pair byte budget to prevent tree operations from throwing StackOverflowError"
@@ -39,67 +40,67 @@ internal class ServeCommand: CliktCommand(
 
     private val serverPort by option(
         help = "Listen to this TCP port",
-        envvar = "THORIUM_SERVER_PORT"
+        envvar = "${envVarPrefix}SERVER_PORT"
     ).int().default(GlobalConfig.defaultServerPort)
 
     private val sourceName by option(
         help = "Reports this string as the 'source' for all internally generated CloudEvents",
-        envvar = "THORIUM_SOURCE_NAME"
+        envvar = "${envVarPrefix}SOURCE_NAME"
     ).default(GlobalConfig.defaultCloudEventSource)
 
     private val queryThreads by option(
         help = "Number of threads to use for queries. Expected to be between 1 and the number of logical processors available",
-        envvar = "THORIUM_QUERY_THREADS"
+        envvar = "${envVarPrefix}QUERY_THREADS"
     ).int().default(GlobalConfig.defaultQueryThreads)
 
     private val translatorThreads by option(
         help = "Number of threads to use for translating ingested data into its index representation. Expected to be between 1 and the number of logical processors available",
-        envvar = "THORIUM_TRANSLATOR_THREADS"
+        envvar = "${envVarPrefix}TRANSLATOR_THREADS"
     ).int().default(GlobalConfig.defaultTranslatorThreads)
 
     private val webServerThreads by option(
         help = "Number of threads to use for the web server. Expected to be between 1 and twice the number of logical processors available",
-        envvar = "THORIUM_WEB_SERVER_THREADS"
+        envvar = "${envVarPrefix}WEB_SERVER_THREADS"
     ).int().default(GlobalConfig.defaultWebServerThreads)
 
     private val maxBodySizeBytes by option(
         help = "Maximum size of all HTTP bodies. Any HTTP request with a body size greater than this value will result in an HTTP 413 result",
-        envvar = "THORIUM_MAX_BODY_SIZE_BYTES"
+        envvar = "${envVarPrefix}MAX_BODY_SIZE_BYTES"
     ).int().default(GlobalConfig.defaultMaxBodySize)
 
     private val maxIdempotencyKeys by option(
         help = "Maximum number of idempotency keys to store before forgetting the oldest key",
-        envvar = "THORIUM_MAX_IDEMPOTENCY_KEYS"
+        envvar = "${envVarPrefix}MAX_IDEMPOTENCY_KEYS"
     ).int().default(GlobalConfig.defaultMaxIdempotencyKeys)
 
     private val maxJsonParsingRecursion by option(
         help = "Maximum stack recursion used by JSON parsing. This is only a performance optimization, and does not prevent deeper JSON nesting than the configured value",
-        envvar = "THORIUM_MAX_JSON_PARSING_RECURSION"
+        envvar = "${envVarPrefix}MAX_JSON_PARSING_RECURSION"
     ).int().default(GlobalConfig.defaultMaxJsonParsingRecursion)
 
     private val maxOutstandingEventsPerQueryThread by option(
         help = "Number of outstanding events per query thread. The global maximum is calculated by multiplying this by the number of query threads",
-        envvar = "THORIUM_MAX_OUTSTANDING_EVENTS_PER_QUERY_THREAD"
+        envvar = "${envVarPrefix}MAX_OUTSTANDING_EVENTS_PER_QUERY_THREAD"
     ).int().default(GlobalConfig.defaultMaxOutstandingEventsPerQueryThread)
 
     private val maxQueryTerms by option(
         help = "Maximum number of terms in a query",
-        envvar = "THORIUM_MAX_QUERY_TERMS"
+        envvar = "${envVarPrefix}MAX_QUERY_TERMS"
     ).int().default(GlobalConfig.defaultMaxQueryTerms)
 
     private val bodyTimeoutMS by option(
         help = "Maximum time (in milliseconds) to allow for HTTP body receipt",
-        envvar = "THORIUM_BODY_TIMEOUT_MS"
+        envvar = "${envVarPrefix}BODY_TIMEOUT_MS"
     ).int().default(GlobalConfig.defaultBodyTimeoutMS)
 
     private val idempotencyExpirationMS by option(
         help = "Lifetime (in milliseconds) of an idempotency key",
-        envvar = "THORIUM_IDEMPOTENCY_EXPIRATION_MS"
+        envvar = "${envVarPrefix}IDEMPOTENCY_EXPIRATION_MS"
     ).int().default(GlobalConfig.defaultIdempotencyExpirationMS)
 
     private val tcpIdleTimeoutMS by option(
         help = "Maximum time (in milliseconds) to wait for TCP activity on an open connection",
-        envvar = "THORIUM_TCP_IDLE_TIMEOUT_MS"
+        envvar = "${envVarPrefix}TCP_IDLE_TIMEOUT_MS"
     ).int().default(GlobalConfig.defaultTcpIdleTimeoutMS)
 
     override fun run() {
