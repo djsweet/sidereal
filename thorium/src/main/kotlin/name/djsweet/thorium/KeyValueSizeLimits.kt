33a7e19258af8105117e5376181a2b98531646b7
Dani Sweet <djsweet@users.noreply.github.com>
2023-12-10 17:47:58 -0500

    Switch Thorium's byte budget to use QPTrie stack safety test

--- thorium/src/main/kotlin/name/djsweet/thorium/KeyValueSizeLimits.kt
+++ thorium/src/main/kotlin/name/djsweet/thorium/KeyValueSizeLimits.kt
@@ -3,87 +3,42 @@ package name.djsweet.thorium
 import io.vertx.core.Vertx
 import io.vertx.kotlin.coroutines.awaitResult
 import kotlinx.coroutines.runBlocking
+import name.djsweet.query.tree.QPTrie
 import kotlin.math.sqrt
 
 // We are unfortunately sometimes extremely stack constrained, especially when running
 // tests with code coverage, so we have to allow for lower key/value sizes than 1KB.
 private const val MIN_POSSIBLE_KEY_VALUE_SIZE = 256
 private const val MAX_POSSIBLE_KEY_VALUE_SIZE = 65536
-// See KeyValueSizeLimitsTest.ts for the mechanism by which we derived this.
-// We previously did not pass in a callback to recurseUntilZero, and the values before
-// that needed to be tuned to prevent test crashes. We started with
-// MAX_POSSIBLE_KEY_VALUE_SIZE_SAFETY_FACTOR=4, an expected safe size of 30857, and an actual
-// safe size when running without coverage of 13620, so we were off by 127%. That would have put
-// us at 10, and when considering the overhead of Radix64Encoding, we'd add another 50% to bring
-// us up to 15. However, this would still crash when trying to run tests with code coverage, so
-// to give us a more realistic expectation of how many stack frames we have, we started passing
-// a callback into recurseUntilZero. This decreased the base number of recursive stack calls,
-// as one would expect, so the safety factor was reduced until we started getting StackOverflowError
-// again.
-private const val MAX_POSSIBLE_KEY_VALUE_SIZE_SAFETY_FACTOR = 13
+
+// This used to be used with a non-allocating recurseUntilZero function, which was a poor simulation of the internals
+// of QPTrie, the implementation that required these limits in the first place. We've since switched to a
+// StackAllocationError test implemented inside QPTrie itself, triggering the exact code that will have issues
+// with stack overflows later. As a result, the safety factor could be reduced to a much smaller float. 1.5x
+// fits with the 33% + overhead increase of Radix64Encoding, and seems to be the least common factor that allows
+// tests to pass across OpenJDK 17, OpenJDK 18, and GraalVM 21. (In the interest of full disclosure: the tests also
+// use an expansion factor of 1.5x for the exact same reason -- this is probably the biggest contributor.)
+private const val MAX_POSSIBLE_KEY_VALUE_SIZE_SAFETY_FACTOR = 1.5
 private const val MAX_POSSIBLE_KEY_VALUE_SIZE_WARMUP_ITERATIONS = 10
 private const val MAX_POSSIBLE_KEY_VALUE_SIZE_ITERATIONS = 60
 
-// We return `result` to ensure that the JVM doesn't attempt dead code elimination.
-// We also pass through an "on max value" handler function, to better simulate the
-// normal use-case of passing through handler functions in QPTrie and QueryTree.
-private fun recurseUntilZero(cur: Int, result: Int, onResult: (result: Int) -> Int): Int {
-    // This is admittedly wasted work, but we want to fill the stack with variables to create a more
-    // realistic testing environment.
-    val curHighest4 = cur.and(0xff.shl(24)).toLong()
-    val curHigher4 = cur.and(0xff.shl(16)).toLong()
-    val curLower4 = cur.and(0xff.shl(8)).toLong()
-    val curLowest4 = cur.and(0xff).toLong()
-    val resultHighest4 = result.and(0xff.shl(24)).toLong()
-    val resultHigher4 = result.and(0xff.shl(16)).toLong()
-    val resultLower4 = result.and(0xff.shl(8)).toLong()
-    val resultLowest4 = result.and(0xff).toLong()
-
-    val curReconstituted = curHighest4.or(curHigher4).or(curLower4).or(curLowest4)
-    val resultReconstituted = resultHighest4.or(resultHigher4).or(resultLower4).or(resultLowest4)
-    return if (cur <= 0) {
-        onResult(resultReconstituted.toInt())
-    } else {
-        recurseUntilZero(curReconstituted.toInt() - 1, result, onResult)
-    }
-}
-
 private fun maxSafeKeyValueSizeSingleIteration(startingUpperBound: Int): Int {
-    var maxBeforeCrash: Int
+    var maxBeforeCrash = 0
     var lowerBound = 0
     var upperBound = startingUpperBound
-    try {
-        var maxAsReportedByHandler = 0
-        maxBeforeCrash = recurseUntilZero(upperBound, upperBound) {
-            maxAsReportedByHandler = it
-            it
-        }
-        // This may seem redundant, but we need to ensure that the handler actually
-        // gets used, and can't be optimized out.
-        maxBeforeCrash = maxBeforeCrash.coerceAtMost(maxAsReportedByHandler)
-        return maxBeforeCrash
-    } catch (e: StackOverflowError) {
-        maxBeforeCrash = 0
+    if (QPTrie.keySizeIsSafeFromOverflow(upperBound)) {
+        return upperBound
     }
     while (lowerBound < upperBound) {
         val testEntry = (upperBound - lowerBound) / 2 + lowerBound
-        try {
-            var maxAsReportedByHandler = 0
-            maxBeforeCrash = recurseUntilZero(testEntry, testEntry) {
-                maxAsReportedByHandler = it
-                it
-            }
-            // This may seem redundant, but we need to ensure that the handler actually
-            // gets used, and can't be optimized out.
-            maxBeforeCrash = maxBeforeCrash.coerceAtMost(maxAsReportedByHandler)
-            // We've already tried the upper bound, and we know that it crashed, so we won't try it again.
-            // We'll deal with the entry we have as the last possible one instead.
+        if (QPTrie.keySizeIsSafeFromOverflow(testEntry)) {
+            maxBeforeCrash = testEntry
             if (testEntry == lowerBound) {
                 break
             } else {
                 lowerBound = testEntry
             }
-        } catch (e: StackOverflowError) {
+        } else {
             upperBound = testEntry
         }
     }
@@ -113,7 +68,7 @@ fun maxKeyValueSizeFromHeapUsage(): Int {
 }
 
 private suspend fun maxSafeKeyValueSizeWithIterationsAsync(vertx: Vertx, iterations: Int): Int {
-    var maxKeySize = maxKeyValueSizeFromHeapUsage() * MAX_POSSIBLE_KEY_VALUE_SIZE_SAFETY_FACTOR
+    var maxKeySize = (maxKeyValueSizeFromHeapUsage() * MAX_POSSIBLE_KEY_VALUE_SIZE_SAFETY_FACTOR).toInt()
     for (i in 0 until iterations) {
         // We can't just .await() under runBlocking, because we've blocked the whole main on that .await(), and
         // that blocked main thread is where the Vertx run loop was going to run.
@@ -124,15 +79,15 @@ private suspend fun maxSafeKeyValueSizeWithIterationsAsync(vertx: Vertx, iterati
             vertx.executeBlocking(executionTarget).andThen(handler)
         }
     }
-    return maxKeySize / MAX_POSSIBLE_KEY_VALUE_SIZE_SAFETY_FACTOR
+    return (maxKeySize / MAX_POSSIBLE_KEY_VALUE_SIZE_SAFETY_FACTOR).toInt()
 }
 
 private fun maxSafeKeyValueSizeWithMaxIterations(): Int {
-    var maxKeySize = maxKeyValueSizeFromHeapUsage() * MAX_POSSIBLE_KEY_VALUE_SIZE_SAFETY_FACTOR
+    var maxKeySize = (maxKeyValueSizeFromHeapUsage() * MAX_POSSIBLE_KEY_VALUE_SIZE_SAFETY_FACTOR).toInt()
     for (i in 0 until MAX_POSSIBLE_KEY_VALUE_SIZE_ITERATIONS) {
         maxKeySize = maxKeySize.coerceAtMost(maxSafeKeyValueSizeSingleIteration(maxKeySize))
     }
-    return maxKeySize / MAX_POSSIBLE_KEY_VALUE_SIZE_SAFETY_FACTOR
+    return (maxKeySize / MAX_POSSIBLE_KEY_VALUE_SIZE_SAFETY_FACTOR).toInt()
 }
 
 private fun ensureMinPossibleKeyValueSize(keySize: Int) {
